0d10f28a472b5be281b82a3d1762aeb58d7e3ec5
voltan <voltan@localhost.localdomain>
2013-03-01 14:09:17 +0330

    Work on modules

---
+++ modules/oledrion/class/oledrion_delivery.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ * oledrion
+ *
+ * @copyright   The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license     http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @author      Hossein Azizabadi (azizabadi@faragostaresh.com)
+ * @version     $Id$
+ */
+
+require 'classheader.php';
+
+class oledrion_delivery extends Oledrion_Object
+{
+	function __construct()
+	{
+		
+	}	
+}
+
+
+class OledrionOledrion_deliveryHandler extends Oledrion_XoopsPersistableObjectHandler
+{
+	function __construct($db)
+	{	//							            Table					Classe				Id
+		parent::__construct($db, 'oledrion_delivery', 'oledrion_delivery', 'delivery_id');
+	}	
+}
+?>
